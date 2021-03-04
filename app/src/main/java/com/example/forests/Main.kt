7aeb7693a1cd579a11768e6e539ecad19d2ede65
The-Anton <shekhar59324@gmail.com>
2021-03-04 12:14:30 +0530

    first commit

--- app/src/main/java/com/example/forests/Main.kt
+++ app/src/main/java/com/example/forests/Main.kt
@@ -6,19 +6,45 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.example.forests.data.stateForestApiService
 import com.example.forests.registerLogin.RegistrationPage
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class Home : AppCompatActivity() {
+class Main : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        setSupportActionBar(toolbar)
-    }
 
+
+        navController = Navigation.findNavController(this,R.id.myNavHostFragment)
+        bottomNavigation.setupWithNavController(navController)
+
+        /* val apiService = stateForestApiService()
+         GlobalScope.launch(Dispatchers.Main){
+             val response = apiService?.getTrees("json",0,1,"Bihar")?.await()
+             val responseText = findViewById<TextView>(R.id.textView2)
+             responseText.text = response!!.records.toString();
+         }*/
+        //val intent = Intent(this, Dashboard::class.java)
+       // startActivity(intent)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
+    }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.nav_menu, menu)
         return super.onCreateOptionsMenu(menu)
