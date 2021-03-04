80cb7d196190b2456e31b601251ab509663ce1be
The-Pascal <chaharchandresh@gmail.com>
2021-03-04 15:35:12 +0530

    Onboarding done - needs to change UI

--- app/src/main/java/com/example/forests/onboarding/screens/Third_Screen.kt
+++ app/src/main/java/com/example/forests/onboarding/screens/Third_Screen.kt
@@ -1,60 +1,32 @@
 package com.example.forests.onboarding.screens
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.forests.R
+import com.example.forests.registerLogin.RegistrationPage
+import kotlinx.android.synthetic.main.fragment_third__screen.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Third_Screen.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Third_Screen : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third__screen, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_third__screen, container, false)
+
+        view.get_started.setOnClickListener {
+            val intent = Intent(activity, RegistrationPage::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or ( Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Third_Screen.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Third_Screen().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
