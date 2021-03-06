6b7d0c92c8a6275ed83b2b8297555d828a1bf729
The-Pascal <chaharchandresh@gmail.com>
2021-03-06 16:12:53 +0530

    Commit to master

--- app/src/main/java/com/example/forests/fragments/Profile.kt
+++ app/src/main/java/com/example/forests/fragments/Profile.kt
@@ -81,7 +81,7 @@ class Profile : Fragment() {
 
             override fun onDataChange(p0: DataSnapshot) {
                 val currentUser = p0.getValue(Users::class.java)
-                view.number_trees_planted_textView8.text = currentUser?.treesPlanted.toString()
+                view.number_trees_planted_textView.text = currentUser?.treesPlanted.toString()
                 view.achievement_name_textView.text = currentUser?.username
                 view.email_profile.text = currentUser?.email
                 Picasso.get().load(currentUser?.imageUrl).into(view.userprofile_imageView)
