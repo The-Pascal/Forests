84dbc731b372ced081510368b1bb4e03e7568559
The-Anton <shekhar59324@gmail.com>
2021-03-06 10:39:53 +0530

    chaged user data class

--- app/src/main/java/com/example/forests/Userdata.kt
+++ app/src/main/java/com/example/forests/Userdata.kt
@@ -3,6 +3,35 @@ package com.example.forests
 import androidx.preference.PreferenceManager
 import org.kodein.di.android.AndroidContextGetter
 
-data class Userdata(var lattitude:String,var longitude:String, var targertrees:Int, var normalizedscore:Int, var plantedtrees:Int,var rating:String){
-    constructor(): this("","",0,0,0,"Rookie")
+data class Userdata(var completedaction:List<Int>,
+                    var lattitude:String,
+                    var longitude:String,
+                    var normalizedscore:Int,
+                    var ongoingaction:List<Int>,
+                    var plantedtrees:Int,
+                    var presentaction:Int,
+                    var rating:String,
+                    var targertrees:Int,
+                    var task1:Int,
+                    var task2:Int,
+                    var task3:Int,
+                    var task4:Int
+
+){
+    constructor(): this(
+        listOf<Int>(0),
+        "",
+        "",
+        0,
+        listOf<Int>(0),
+        0,
+        0,
+        "Rookie",
+        0,
+        0,
+        0,
+        0,
+        0
+
+    )
 }
